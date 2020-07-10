5f728d5507c69ff46747b5e1386159d9c706f2f2
Sina Honarvar <sina.ho79@gmail.com>
2020-07-10 23:29:09 +0430

    Update install-mautic.sh

--- install-mautic.sh
+++ install-mautic.sh
@@ -31,13 +31,13 @@ unzip /var/temp.mautic/mautic.zip -d /var/www/html/
 mv /var/www/html/mautic-master/* /var/www/html/
 rm -rfd /var/www/html/mautic-master/
 rm /var/temp.mautic/mautic.zip
-chown -R www-data:www-data /var/www/html/
-chmod -R 777 /var/www/html/
+
+
 apt install -y composer
 cd /var/www/html
 apt install -y  php7.2-curl  php7.2-xml  php7.2-mbstring php7.2-zip php7.2-bcmath
 composer install 
-chown -R www-data /var/www/html/
+chown -R www-data:www-data /var/www/html/
 systemctl restart apache2
 chmod -R 777 /var/www/html
 
